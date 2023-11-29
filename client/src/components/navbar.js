fa260530e3fa0eb611bc3833e64dbe0e5581fc48
mridul saggi <91930535+mridulsaggi@users.noreply.github.com>
2023-11-29 10:48:32 +0530

    updated

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const navbar = () => {
+const Navbar = () => {
   return (
     <div>
       navbar
@@ -8,4 +8,4 @@ const navbar = () => {
   )
 }
 
-export default navbar
+export default Navbar
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Navbar from './components/navbar';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Navbar/>
+    <Routes>
+      <Route path='/' element={<App />} />
+    </Routes>
+  </Router>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 );
