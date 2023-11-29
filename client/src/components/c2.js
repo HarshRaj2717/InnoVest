fa08ec313493f0e1bb9ecf99fd9895f9919c7b38
harshraj2717 <harshraj2717@gmail.com>
2023-11-30 01:12:58 +0530

    minor UI improvements

--- client/src/components/c2.js
+++ client/src/components/c2.js
@@ -8,7 +8,7 @@ const Two = () => {
       <div class="skill-card">
                     <header class="skill-card__header"><img class="skill-card__icon" src={judge} alt="HTML5 Logo" /></header>
                     <section class="skill-card__body">
-                        <h2 class="skill-card__title">Virtual SharkTank</h2><span class="skill-card__duration">letsgo boyy</span>
+                        <h2 class="skill-card__title">SharkTank</h2><span class="skill-card__duration">letsgo boyy</span>
                         <ul class="skill-card__knowledge">
                             <li>Evaluate your startup </li>
                             <li>get detailed review</li>
