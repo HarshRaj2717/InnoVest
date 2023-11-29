7996fe0758d09d8c8d2911c598f64a6cb523715b
harshraj2717 <harshraj2717@gmail.com>
2023-11-29 10:58:12 +0530

    add tailwind + daisyui

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import './index.css';
+import './dist/output.css'
 import App from './App';
 import Navbar from './components/navbar';
 const root = ReactDOM.createRoot(document.getElementById('root'));
