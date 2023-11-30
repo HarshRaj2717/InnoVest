f73417e12c4ae3168ed73fbc87e0f55bdaa45930
harshraj2717 <harshraj2717@gmail.com>
2023-11-30 13:42:40 +0530

    quick fix for shark tank

--- client/src/components/videoconf/live.js
+++ client/src/components/videoconf/live.js
@@ -1,35 +1,35 @@
-import React from 'react'
-import "./live.css"
-import { Link } from 'react-router-dom'
-const Live = () => {
-    return (
-        <div className='hehe'>
-            <div className='flex justify-center text-2xl m-4 underline'>Live Events</div>
-            <ol>
-                <Link to="/video">
-                    <li >
-                        <div class="icon"><i class="fa-solid fa-bicycle"></i></div>
-                        <div class="title">SHARK TANK 1</div>
-                        <div class="descr">Luis Pillich</div>
-                    </li></Link>
-                <Link to="/video">
-                    <li >
-                        <div class="icon"><i class="fa-solid fa-car"></i></div>
-                        <div class="title">SHARK TANK 2</div>
-                        <div class="descr">Andrew Masotta</div>
-                    </li>
-                </Link>
-                <Link to="/video">
-                    <li >
-                        <div class="icon"><i class="fa-solid fa-helicopter"></i></div>
-                        <div class="title">SHARK TANK 3</div>
-                        <div class="descr">Michael Krakovsky</div>
-                    </li>
-                </Link>
-            </ol>
+// import React from 'react'
+// // import "./live.css"
+// import { Link } from 'react-router-dom'
+// const Live = () => {
+//     return (
+//         <div className='hehe'>
+//             <div className='flex justify-center text-2xl m-4 underline'>Live Events</div>
+//             <ol>
+//                 <Link to="/video">
+//                     <li >
+//                         <div class="icon"><i class="fa-solid fa-bicycle"></i></div>
+//                         <div class="title">SHARK TANK 1</div>
+//                         <div class="descr">Luis Pillich</div>
+//                     </li></Link>
+//                 <Link to="/video">
+//                     <li >
+//                         <div class="icon"><i class="fa-solid fa-car"></i></div>
+//                         <div class="title">SHARK TANK 2</div>
+//                         <div class="descr">Andrew Masotta</div>
+//                     </li>
+//                 </Link>
+//                 <Link to="/video">
+//                     <li >
+//                         <div class="icon"><i class="fa-solid fa-helicopter"></i></div>
+//                         <div class="title">SHARK TANK 3</div>
+//                         <div class="descr">Michael Krakovsky</div>
+//                     </li>
+//                 </Link>
+//             </ol>
 
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
 
-export default Live
+// export default Live
